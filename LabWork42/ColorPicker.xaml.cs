c1b40930753aea51e021a8a61111f6a11b2ccde4
0109-04 <0109-04@0109-04>
2025-06-16 11:18:37 +0300

    Р”РѕР±Р°РІСЊС‚Рµ С„Р°Р№Р»С‹ РїСЂРѕРµРєС‚Р°.

---
+++ LabWork42/ColorPicker.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace LabWork42
+{
+    /// <summary>
+    /// Логика взаимодействия для ColorPicker.xaml
+    /// </summary>
+    public partial class ColorPicker : Window
+    {
+        public ColorPicker()
+        {
+            InitializeComponent();
+            TestColorRectangle.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+        private byte _red { get; set; }
+        private byte _green { get; set; }
+        private byte _blue { get; set; }
+
+        public byte Red
+        {
+            get => _red;
+        }
+        public byte Green
+        {
+            get => _green;
+        }
+        public byte Blue
+        {
+            get => _blue;
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            ChangeBackground();
+        }
+
+        private void ColorCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            ChangeBackground();
+        }
+
+        private void ColorCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            ChangeBackground();
+        }
+
+        private void ChangeBackground()
+        {
+            if (!IsLoaded)
+                return;
+            if (!RedColorCheckBox.IsChecked.Value)
+                _red = 0;
+            else
+                _red = (byte)Double.Parse(RedColorSlider.Value.ToString());
+
+            if (!BlueColorCheckBox.IsChecked.Value)
+                _blue = 0;
+            else
+                _blue = (byte)Double.Parse(BlueColorSlider.Value.ToString());
+            if (!GreenColorCheckBox.IsChecked.Value)
+                _green = 0;
+            else
+                _green = (byte)Double.Parse(GreenColorSlider.Value.ToString());
+            TestColorRectangle.Fill = new SolidColorBrush(Color.FromRgb(_red, _green, _blue));
+        }
+
+        private void OKButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+    }
+}
+
---
+++ LabWork42/Task1.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Win32;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace LabWork42
+{
+    /// <summary>
+    /// Логика взаимодействия для Task1.xaml
+    /// </summary>
+    public partial class Task1 : Window
+    {
+        public Task1()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangeImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new();
+            if (openFileDialog.ShowDialog() == false)
+                return;
+
+            inkCanvas.Children.Clear();
+
+            var bitmapImage = new BitmapImage(new Uri(openFileDialog.FileName));
+            var image = new Image();
+            image.Source = bitmapImage;
+            inkCanvas.Children.Add(image);
+        }
+
+        private void ChangeColorButton_Click(object sender, RoutedEventArgs e)
+        {
+            ColorPicker picker = new ColorPicker();
+            if (picker.ShowDialog() == false)
+                return;
+            inkCanvas.DefaultDrawingAttributes.Color = Color.FromRgb(picker.Red, picker.Green, picker.Blue);
+        }
+
+        private void FontSizeTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int size;
+            if (!int.TryParse(FontSizeTextBox.Text, out size))
+                return;
+
+            inkCanvas.DefaultDrawingAttributes.Width = inkCanvas.DefaultDrawingAttributes.Height = size;
+        }
+    }
+}
